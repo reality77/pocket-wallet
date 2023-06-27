d07be41fb8492c67094bce902a5ea00ce66d7ad0
reality77 <davmail08-misc@yahoo.fr>
2023-06-27 13:33:30 +0200

    Redesign admin app

--- vapp/pocket-wallet-admin/src/components/PocketWalletController.vue
+++ vapp/pocket-wallet-admin/src/components/PocketWalletController.vue
@@ -1,127 +1,169 @@
 <template>
-    <div>
-        <h2>Contract</h2>
-        <div>
-            <div class="mt-2">
-                <label>Address : </label><span class="data">{{ contract_address_short }}</span>
+    <div class="flex flex-col">
+        <div class="border-slate-600 border-b pb-4 mb-4">
+            <h2>
+                <span>Contract</span> <span class="data">{{ contract_address }}</span>
+            </h2>
+        </div>
+        <div class="p-4">
+            <div>
+                <span class="mr-2">
+                    <i class="las la-coins la-3x"></i>
+                </span>
+                <span class="data text-3xl">{{ contract_balance }} Ξ</span>
             </div>
-            <div class="mt-2">
-                <label>Balance : </label><span class="data">{{ contract_balance }} Ξ</span>
-            </div>
-            <div class="mt-2">
-                <label>User : </label><span class="data">{{ user_address_short }}</span>
-            </div>
-            <div v-if="is_controller" class="mt-2 flex flex-col">
-                <div>
-                    <h3>Add funds</h3>
-                    <div class="mt-2">
-                        <label>Amount : </label><input type="number" v-model="amount" class="ml-2" />
-                        <PromiseButton :promiseFunction="addFunds" childClass="ml-2">Add funds</PromiseButton>
-                    </div>
-                </div>
-                <div>
-                    <h3>Replace user</h3>
-                    <div class="mt-2">
-                        <label>New user address : </label><input v-model="newUserAddress" class="ml-2" />
-                        <PromiseButton :promiseFunction="registerUser" childClass="ml-2">Replace user</PromiseButton>
-                    </div>
-                </div>
-                <div>
-                    <h3>Receipients</h3>
-                    <div class="mt-2">
-                        <ul class="grid grid-rows-1 divide-y divide-slate-700">
-                            <li v-for="receipient in list_receipients" :key="receipient.receipient" class="p-2">
-                                <p>
-                                    <span class="">{{ receipient.label }}</span>&nbsp;
-                                    <span class="">{{ receipient.receipient }}</span>
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="mt-2">
-                        <label>Label : </label><input v-model="newReceipientLabel" class="ml-2" />
-                        <label>Address : </label><input v-model="newReceipientAddress" class="ml-2" />
-                        <PromiseButton :promiseFunction="addReceipient" childClass="ml-2">Add receipient</PromiseButton>
-                    </div>
-                </div>
-            </div>
-            <div v-else>
-                <span class="text-red-500">You are not a controller of this contract</span>
+            <div class="mt-4">
+                <label class="text-sm">User wallet address : </label><span class="data text-sm">{{ user_address }}</span>
             </div>
         </div>
+        <div v-if="is_controller">
+            <div class="p-4">
+                <div class="text-xl font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+                    <ul ref="tabs" class="flex flex-wrap -mb-px">
+                        <li class="mr-2">
+                            <a href="#" @click="selectTab(0)" :class="selected_tab_index == 0 ? getActiveTabClass() : getInactiveTabClass()" :aria-current="selected_tab_index == 0 ? 'page' : ''">Add funds</a>
+                        </li>
+                        <li class="mr-2">
+                            <a href="#" @click="selectTab(1)" :class="selected_tab_index == 1 ? getActiveTabClass() : getInactiveTabClass()" :aria-current="selected_tab_index == 1 ? 'page' : ''">Replace user wallet</a>
+                        </li>
+                        <li class="mr-2">
+                            <a href="#" @click="selectTab(2)" :class="selected_tab_index == 2 ? getActiveTabClass() : getInactiveTabClass()" :aria-current="selected_tab_index == 2 ? 'page' : ''">Receipients</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div v-if="selected_tab_index == 0">
+                    <h3>Add funds to the user contract</h3>
+                    <div class="mt-2 flex flex-col">
+                        <div class="flex-auto">
+                            <input type="number" v-model="amount" class="bg-transparent text-white border-slate-700 text-center text-2xl w-48">
+                            <span class="text-white text-2xl">Ξ</span>
+                        </div>
+                        <div class="mt-2">
+                            <PromiseButton :promiseFunction="addFunds">Add funds</PromiseButton>
+                        </div>
+                    </div>
+                </div>
+                <div v-if="selected_tab_index == 1">
+                    <h3>Replace user</h3>
+                    <div class="mt-2 flex flex-col">
+                        <div class="flex-auto">
+                            <label>New user address : </label>
+                            <input v-model="newUserAddress" class="bg-transparent text-white border-slate-700 text-center w-48" />
+                        </div>
+                        <div class="mt-2">
+                            <PromiseButton :promiseFunction="registerUser">Replace user</PromiseButton>
+                        </div>
+                    </div>
+                </div>
+                <div v-if="selected_tab_index == 2">
+                    <h3>Receipients</h3>
+                    <div class="mt-2 grid grid-cols-2 gap-2">
+                        <template v-for="receipient in list_receipients" :key="receipient.receipient">
+                            <div>
+                                <span class="data font-bold">{{ receipient.label }}</span>
+                            </div>
+                            <div>
+                                <span class="data">{{ receipient.receipient }}</span>
+                            </div>
+                        </template>
+                        <div>
+                            <label>Label : </label><input v-model="newReceipientLabel" class="ml-2" />
+                        </div>
+                        <div>
+                            <label>Address : </label><input v-model="newReceipientAddress" class="ml-2" />
+                            <PromiseButton :promiseFunction="addReceipient" childClass="ml-2">Add receipient</PromiseButton>
+                        </div>            
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div v-else>
+            <span class="text-red-500">You are not a controller of this contract</span>
+        </div>        
     </div>
 </template>
 
-<script>
+<script setup>
 import { ethers } from "ethers";
 import PromiseButton from "./PromiseButton.vue"
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
 
-export default {
-    name: 'PocketWalletController',
-    data: function () {
-        return {
-            amount: 0,
-            newUserAddress: null,
-            newReceipientLabel: null,
-            newReceipientAddress: null,
-        }
-    },
-    props: {
-    },
-    computed: {
-        contract_balance() {
-            if(this.$store.getters.contract_balance != null) {
-                return ethers.utils.formatEther(this.$store.getters.contract_balance);
+const store = useStore();
+
+const tabs = ref()
+
+const amount = ref(0)
+const newUserAddress = ref()
+const newReceipientLabel = ref()
+const newReceipientAddress = ref()
+
+const contract_balance = computed(() => {
+            if(store.getters.contract_balance != null) {
+                return ethers.utils.formatEther(store.getters.contract_balance);
             } else {
                 return null;
             }
-        },
-        contract_address() {
-            return this.$store.getters.contract_address;
-        },
-        contract_address_short() 
-        {
-            return `${this.$store.getters.contract_address.substr(0, 6)}...${this.$store.getters.contract_address.substr(-4)}`;
-        },
-        user_address() {
-            return this.$store.getters.user_address;
-        },
-        user_address_short() 
-        {
-            if (this.$store.getters.user_address) {
-                return `${this.$store.getters.user_address.substr(0, 6)}...${this.$store.getters.user_address.substr(-4)}`;
+        });
+
+
+const contract_address = computed(() => {
+            return store.getters.contract_address;
+        });
+
+/*
+const contract_address_short = computed(() => {
+            return `${store.getters.contract_address.substr(0, 6)}...${store.getters.contract_address.substr(-4)}`;
+        });
+*/
+
+const user_address = computed(() => {
+            return store.getters.user_address;
+        });
+
+        /*
+const user_address_short = computed(() => {
+            if (store.getters.user_address) {
+                return `${store.getters.user_address.substr(0, 6)}...${store.getters.user_address.substr(-4)}`;
             } else {
                 return ''
             }
-        },    
-        is_controller() {
-            return this.$store.getters.is_controller;
-        },
-        list_receipients() {
-            return this.$store.getters.list_receipients;
-        },        
-    },
-    components: {
-        PromiseButton
-    },
-    methods: {
-        async addFunds() {
-            await this.$store.dispatch("addFunds", this.$data.amount.toString());
-        },
-        async registerUser() {
-            await this.$store.dispatch("registerUser", this.$data.newUserAddress);
-        },
-        async addReceipient() {
-            await this.$store.dispatch("addReceipient", { label: this.$data.newReceipientLabel, receipient: this.$data.newReceipientAddress });
-        }   
-    },
-    emits: {
-    },
-    async mounted() {
+        });
+*/
 
-        //var bus = this
-        //bus.$store.dispatch("initializeProvider");
-    },
+const is_controller = computed(() => store.getters.is_controller);
+
+const list_receipients = computed(() => store.getters.list_receipients);
+
+const selected_tab_index = ref(0);
+
+
+function getInactiveTabClass() {
+    return "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-gray-300 hover:text-gray-300";
+}
+
+function getActiveTabClass() {
+    return "inline-block p-4 border-b-2 rounded-t-lg active text-purple-500 border-purple-500";
+}
+
+
+async function addFunds() {
+    await store.dispatch("addFunds", amount.value.toString());
+}
+
+async function registerUser() {
+    await store.dispatch("registerUser", newUserAddress.value);
+}
+
+async function addReceipient() {
+    await store.dispatch("addReceipient", { label: newReceipientLabel.value, receipient: newReceipientAddress.value });
+}
+
+function selectTab(index) {
+    selected_tab_index.value = index;
+
 }
 
 </script>
